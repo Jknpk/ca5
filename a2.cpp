643451d37864a50faeff6269f60cd8bd7985e312
Jannis <j.konopka@fz-juelich.de>
2015-11-10 11:02:53 +0100

    Aufgabe 1 und 2 fit gemacht

--- a2.cpp
+++ a2.cpp
@@ -1,23 +1,18 @@
 #include<iostream>
 #include "array2d.h"
 using namespace std;
-
-
 int main(){
   
-  
-  int n = 3;
-  int m = 5;
-  int p = 5;
-  int q = 7;
-
-  
+  int n = 5;
+  int m = 4;
+  int p = 4;
+  int q = 9;
   
   Array2d<int> matrix1(n,m,3);
   Array2d<int> matrix2(p,q,4);
   
   // Spaltenanzahl der ersten Matrix muss gleich der Zeilenanzahl der 2. Matrix sein
-  // Ergebnismatrix ist dann Zeilenahl der ersten und Spaltenzahl der 2. Matrix
+  // Ergebnismatrix ist dann Zeilenzahl der ersten und Spaltenzahl der 2. Matrix
 
   //matrix3 = matrix1 * matrix2
 
@@ -30,28 +25,20 @@ int main(){
   int r = n;
   int s = q;  
   Array2d<int> matrix3(r,s);
-  
-  
+ // int summe = 0;
   for(int i = 0; i < r; i++){
     for(int j = 0; j < s; j++){
-	
+	matrix3.at(i,j) = 0;
       for(int k = 0; k < m; k++){   // m und p sind gleich!
-	
-	     
-
-      
-      
-    
-    
-    
+		matrix3.at(i,j) = (matrix3.at(i,j) + matrix1.at(i, k) * matrix2.at(k, j));    
       }
-      
-      matrix3.at(i,j)=;
-      
     }
   }
-  
-  
+  printArray(matrix1);
+  cout << endl << "    X    " << endl << endl;
+  printArray(matrix2);
+  cout << endl << "    =    " << endl << endl;
+  printArray(matrix3);
   
   return 0;
   
